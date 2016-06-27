8279e1a2751f07351498cb07d27b1ff645c8504d
Lewis Moten <lewismoten@gmail.com>
2016-06-27 15:44:44 -0400

    Use Hyperbolic Tangent from npm

--- run.js
+++ run.js
@@ -1,6 +1,7 @@
 
 var sigmoid = require('sigmoid');
 var hyperbolicTangent = require('./lib/hyperbolic-tangent.js');
+var htan = require('htan');
 var softmax = require('softmax-fn');
 var heaviside = require('heaviside');
 
@@ -11,9 +12,10 @@ var v = vv[0] || 0;
 // console.log('Sigmoid', sigmoid(v));
 // console.log('Sigmoid derivitive', sigmoid(v, true));
 // console.log('Heaviside step', heaviside(v));
-// console.log('Hyperbolic Tangent', hyperbolicTangent(v));
+console.log('Hyperbolic Tangent', hyperbolicTangent(v));
+console.log('Hyperbolic Tangent', htan(v));
 // console.log();
 // console.log('values', JSON.stringify(vv));
 // console.log('Softmax', softmax(vv));
 
-console.log(sigmoid(v));
+//console.log(sigmoid(v));

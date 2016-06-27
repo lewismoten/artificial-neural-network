3e43bc1e291e1839c15171e1c704c5e5d0f94505
Lewis Moten <lewismoten@gmail.com>
2016-06-27 15:31:34 -0400

    use npm for heaviside step function

--- run.js
+++ run.js
@@ -1,5 +1,5 @@
 var sigmoid = require('./lib/sigmoid.js');
-var heavsideStep = require('./lib/heavside-step.js');
+var heaviside = require('heaviside');
 var hyperbolicTangent = require('./lib/hyperbolic-tangent.js');
 var softmax = require('softmax-fn');
 
@@ -9,7 +9,7 @@ var v = vv[0] || 0;
 console.log('value', v);
 console.log('Sigmoid', sigmoid(v));
 console.log('Sigmoid derivitive', sigmoid(v, true));
-console.log('Heavside step', heavsideStep(v));
+console.log('Heaviside step', heaviside(v));
 console.log('Hyperbolic Tangent', hyperbolicTangent(v));
 console.log();
 console.log('values', JSON.stringify(vv));

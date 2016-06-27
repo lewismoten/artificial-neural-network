c875916614244ecfa8bc69717487fe6b06c3a502
Lewis Moten <lewismoten@gmail.com>
2016-06-27 15:38:09 -0400

    get sigmoid from npm

--- run.js
+++ run.js
@@ -1,17 +1,19 @@
-var sigmoid = require('./lib/sigmoid.js');
-var hyperbolicTangent = require('./lib/hyperbolic-tangent.js');
 
+var sigmoid = require('sigmoid');
+var hyperbolicTangent = require('./lib/hyperbolic-tangent.js');
 var softmax = require('softmax-fn');
 var heaviside = require('heaviside');
 
 var vv = process.argv.slice(2).map(function(n) { return Number(n);});
 var v = vv[0] || 0;
+//
+// console.log('value', v);
+// console.log('Sigmoid', sigmoid(v));
+// console.log('Sigmoid derivitive', sigmoid(v, true));
+// console.log('Heaviside step', heaviside(v));
+// console.log('Hyperbolic Tangent', hyperbolicTangent(v));
+// console.log();
+// console.log('values', JSON.stringify(vv));
+// console.log('Softmax', softmax(vv));
 
-console.log('value', v);
-console.log('Sigmoid', sigmoid(v));
-console.log('Sigmoid derivitive', sigmoid(v, true));
-console.log('Heaviside step', heaviside(v));
-console.log('Hyperbolic Tangent', hyperbolicTangent(v));
-console.log();
-console.log('values', JSON.stringify(vv));
-console.log('Softmax', softmax(vv));
+console.log(sigmoid(v));

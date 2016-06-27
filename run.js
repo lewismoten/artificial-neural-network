5a1f77d90abb8049ba4d4b8f735a1825f31c53b2
Lewis Moten <lewismoten@gmail.com>
2016-06-27 18:02:13 -0400

    create a neuron

--- run.js
+++ run.js
@@ -1,21 +1,22 @@
 
 var sigmoid = require('sigmoid');
-var hyperbolicTangent = require('./lib/hyperbolic-tangent.js');
 var htan = require('htan');
 var softmax = require('softmax-fn');
 var heaviside = require('heaviside');
+var getNeuron = require('./lib/neuron');
 
 var vv = process.argv.slice(2).map(function(n) { return Number(n);});
 var v = vv[0] || 0;
-//
+
 // console.log('value', v);
 // console.log('Sigmoid', sigmoid(v));
 // console.log('Sigmoid derivitive', sigmoid(v, true));
 // console.log('Heaviside step', heaviside(v));
-console.log('Hyperbolic Tangent', hyperbolicTangent(v));
-console.log('Hyperbolic Tangent', htan(v));
+// console.log('Hyperbolic Tangent', htan(v));
 // console.log();
 // console.log('values', JSON.stringify(vv));
 // console.log('Softmax', softmax(vv));
 
 //console.log(sigmoid(v));
+var n = getNeuron(3,4,2);
+console.log(n.toString());

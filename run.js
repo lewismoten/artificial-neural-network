35d2840ec9d50c22ee2b7783d882287de7c327c2
Lewis Moten <lewismoten@gmail.com>
2016-07-03 09:56:29 -0400

    wip - updating hidden weights. broken, adding context tosoftmax-fn

--- run.js
+++ run.js
@@ -1,7 +1,11 @@
 
 const lib = require('./lib');
 
-let xor = lib.create(2, 2, 1);
+let xor = lib.create(2, 2, 1),
+  LearningRate = 0.03,
+  momentum = 0.1,
+  input = [1, 1],
+  expectedOutput = [1];
 
 xor[0].weights = [
   [2, -2],
@@ -16,9 +20,9 @@ xor[2].biases = [-3];
 xor[1].activate = 'htan';
 xor[2].activate = 'heaviside';
 
-xor[0].values = [1, 1];
+xor[0].values = input;
 let result = lib.runner(xor)[0];
-lib.learn(xor, [1]);
+lib.learn(xor, expectedOutput, LearningRate, momentum);
 
 // console.log(JSON.stringify(network, null, ' '));
 // console.log('result', JSON.stringify(output));
